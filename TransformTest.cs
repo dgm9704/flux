dd44af51e279fcfecb92150cb229f062551cc0f8
John Nordberg <john.nordberg@gmail.com>
2020-05-31 22:25:35 +0300

    playing around with aifm

--- TransformTest.cs
+++ TransformTest.cs
@@ -1,13 +1,25 @@
 namespace flux
 {
+    using System.Xml.Xsl;
     using Xunit;
 
     public class TransformTest
     {
         [Fact]
-        public void Test1()
+        public void BooksTest()
         {
-
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load("output.xsl");
+            xslt.Transform("books.xml", "books.html");
         }
+
+        [Fact]
+        public void AifmTest()
+        {
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load("aifm.xsl");
+            xslt.Transform("aifmsample.xml", "aifmoutput.txt");
+        }
+
     }
 }
